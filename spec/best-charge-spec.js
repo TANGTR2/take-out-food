7399969d75710bda4bd2e33e68e4e6b21426efe5
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-08 12:14:56 +0800

    [Tracy] finish function spiltIdAndCount

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,48 +1,68 @@
-describe('Take out food', function () {
+'use strict';
 
-  it('should generate best charge when best is 指定菜品半价', function() {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-黄焖鸡 x 1 = 18元
-肉夹馍 x 2 = 12元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-指定菜品半价(黄焖鸡，凉皮)，省13元
------------------------------------
-总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+const {spiltIdAndCount} = require('../src/best-charge')
+
+describe('Unit Test', function() {
+
+  it('should spilt id and count', function() {
+    //given
+    const selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    //when
+    const idAndCounts = spiltIdAndCount(selectedItems);
+    //then
+    let result = JSON.stringify([
+      {"id":"ITEM0001","count":1},
+      {"id":"ITEM0013","count":2},
+      {"id":"ITEM0022","count":1}
+    ]);
+    expect(JSON.stringify(idAndCounts)).toBe(result)
   });
-
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-满30减6元，省6元
------------------------------------
-总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
-
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
------------------------------------
-总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
-
 });
+
+// describe('Take out food', function () {
+
+//   it('should generate best charge when best is 指定菜品半价', function() {
+//     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 黄焖鸡 x 1 = 18元
+// 肉夹馍 x 2 = 12元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 指定菜品半价(黄焖鸡，凉皮)，省13元
+// -----------------------------------
+// 总计：25元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when best is 满30减6元', function() {
+//     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 满30减6元，省6元
+// -----------------------------------
+// 总计：26元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when no promotion can be used', function() {
+//     let inputs = ["ITEM0013 x 4"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// -----------------------------------
+// 总计：24元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+// });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,20 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+'use strict';
+
+// function bestCharge(selectedItems) {
+// }
+
+function spiltIdAndCount(selectedItems){
+  let idAndCounts = [];
+  for(let item of selectedItems){
+    let idAndCount = {};
+    let div = item.split(" ");
+    idAndCount.id = div[0];
+    idAndCount.count = parseFloat(div[2]);
+    idAndCounts.push(idAndCount);
+  }
+  //console.info(idAndCounts);
+  return idAndCounts;
 }
+
+module.exports={
+  spiltIdAndCount}
